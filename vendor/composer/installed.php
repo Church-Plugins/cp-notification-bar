fcae9b18d9dfe196715372f674fbe52679d4560b
Tanner Moushey <tanner@missionlab.dev>
2023-02-16 10:35:30 -0600

    add vendor file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e38ab35657d2fe5e6fc5c8a9e2925cf5e1a12629',
+    'name' => 'churchplugins/cp-notification-bars',
+  ),
+  'versions' => 
+  array (
+    'churchplugins/cp-notification-bars' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e38ab35657d2fe5e6fc5c8a9e2925cf5e1a12629',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a5604f5ab3bd5f000e503dda02603e15e534d47',
+    ),
+  ),
+);
